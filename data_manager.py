ec37febf3d585a185294d1d1658252988d305a83
idzia <ania.idzi@gmail.com>
2018-02-20 11:40:07 +0100

    Add data_manager, import/export function

---
+++ data_manager.py
@@ -0,0 +1,24 @@
+""" f. odczytuje z pliku
+parametry:
+filename -> "expenses.txt" """
+
+def data_import(filename="expenses.txt" ):
+    with open(filename, "r") as file:
+        content = file.readlines()
+    content = list(map(str.strip, content))
+    content = [element.split(",") for element in content]
+    return content
+
+
+""" funkcja nadpisuje dane (mode = w) w pliku o podanej przez użytkownika nazwie 
+    parametry:
+    data -> lista list, 
+    filename -> earnings.txt / expences.txt / categories.txt """
+
+def data_export(data, filename):
+    with open(filename, "w") as file:
+        for i in range(len(data)):
+            data[i] = list(map(str, data[i]))
+            row = ",".join(data[i])
+            file.write(row + "\n")
+
