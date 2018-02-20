58557e9c9063dd0eb456bf5cacdde63be4bf8814
idzia <ania.idzi@gmail.com>
2018-02-20 14:55:28 +0100

    Add function to print table

--- display.py
+++ display.py
@@ -13,3 +13,23 @@ def print_data(data, header):
 
 def print_message(message):
     print(message)
+
+
+def print_table(data, title):
+    lenght = [6, 8, 15, 30, 6, 4, 4]
+    additional_characters = 16
+    width = sum(lenght) + additional_characters
+    row = []
+    
+    for i in range(len(title)):
+        row.append("| " + title[i].center(lenght[i]))
+    row.append(" |")
+    print("".join(row))
+    print("="*width)
+
+    for item in data:    
+        row = []
+        for i in range(len(title)):
+            row.append("| " + item[i].rjust(lenght[i]))
+        row.append(" |")
+        print("".join(row))
