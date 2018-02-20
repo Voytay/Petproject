9f84d87cb68c423b547fb86a85fcb9287622c0e8
Jakub Wojtasik <jakubwojtasikzsp@gmail.com>
2018-02-20 11:01:31 +0100

    Add function to display - print data

--- display.py
+++ display.py
@@ -2,4 +2,9 @@ def print_menu(data):
     i = 1
     for element in data:
         print(i, "----->", element.title())
-        i +=1
+        i += 1
+
+def print_data(data, header):
+    print(header)
+    for element in data:
+        print(" | ".join(element))
