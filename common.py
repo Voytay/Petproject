52b83ea9239dc3b5278a9cab33d254810a22d5f4
idzia <ania.idzi@gmail.com>
2018-02-20 14:52:40 +0100

    Add function generate_id

--- common.py
+++ common.py
@@ -1,3 +1,6 @@
+import random
+import string
+
 def get_decision_input(dec):
     while dec is not True:
         try:
@@ -5,3 +8,20 @@ def get_decision_input(dec):
             return dec
         except:
             pass
+
+
+def generate_id(data):
+    sign = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "+"]  
+    unique = False
+    while unique == False:
+        ids = "".join([random.choice(string.ascii_lowercase), 
+                    random.choice(sign),
+                    random.choice(string.digits),
+                    random.choice(string.ascii_uppercase)])
+        unique = True
+        
+        ids_index = 0
+        for row in data:
+            if row[ids_index] == ids:
+                unique = False
+    return ids
