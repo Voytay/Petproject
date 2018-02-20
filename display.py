b549cb4045ee9d7ee10323c7d96d0cf25d05814d
Jakub Wojtasik <jakubwojtasikzsp@gmail.com>
2018-02-20 13:43:14 +0100

    Fixes to common functions

--- display.py
+++ display.py
@@ -4,7 +4,12 @@ def print_menu(data):
         print(i, "----->", element.title())
         i += 1
 
+
 def print_data(data, header):
     print(header)
     for element in data:
         print(" | ".join(element))
+
+
+def print_message(message):
+    print(message)
