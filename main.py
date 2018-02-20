f8ccbe3229a0891a228b49611ae6de0ef220e568
Jakub Wojtasik <jakubwojtasikzsp@gmail.com>
2018-02-20 13:42:26 +0100

    Fixes in function calls

--- main.py
+++ main.py
@@ -1,29 +1,32 @@
 import display
 import common
 import income_expenses
+from config import config
 
 def main():
-    menu = ['Expenses', 'Incomes', 'Raports', '###Config'] # main menu
-    display.print_menu(menu)
-    dec = 0
-    dec = common.get_decision_input(dec)
-    #header = 'HEADER----------------H'
-    #data = [['1','2000','data','kategoria']]
-    #display.print_data(data, header)
-    if dec == 1:
-        #income_expenses.xxx
-        pass
-    elif dec == 2:
-        #income_expenses.yyy
-        pass
-    elif dec == 3:
-        # raport module
-        pass
-    elif dec == 4:
-        # config module
-        pass
-    else:
-        print("Wrong key!!!!")
+    while True:
+        menu = ['Expenses', 'Incomes', 'Raports', '###Config', 'Exit'] # main menu
+        display.print_menu(menu)
+        dec = 0
+        dec = common.get_decision_input(dec)
+        #header = 'HEADER----------------H'
+        #data = [['1','2000','data','kategoria']]
+        #display.print_data(data, header)
+        if dec == 1:
+            #income_expenses.xxx
+            pass
+        elif dec == 2:
+            #income_expenses.yyy
+            pass
+        elif dec == 3:
+            # raport module
+            pass
+        elif dec == 4:
+            config()
+        elif dec == 5:
+            return False
+        else:
+            display.print_message("No option!")
 
 if __name__ == '__main__':
     main()
