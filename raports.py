63557e90a6839fe3a912e67dd8eb6474dda1e639
Jakub Wojtasik <jakubwojtasikzsp@gmail.com>
2018-02-21 13:55:07 +0100

    Add functions to raports module

--- raports.py
+++ raports.py
@@ -12,7 +12,7 @@ def raport():
         if decision == 1:
             gen_raport()
         elif decision == 2:
-            show_list_rap()
+            show_list_raport()
         elif decision == 3:
             delete_raport()
 
@@ -26,6 +26,28 @@ def gen_raport():
         decision = common.get_decision_input(decision)
         if decision == 1:
             data = data_manager.data_import('earnings.txt')
-            common.sort(data)
+            data = common.sort(data)
+            display.print_table(data,'')
+            save_raport(data)
         elif decision == 2:
-            pass
+            data = data_manager.data_import('expenses.txt')
+            data = common.sort(data)
+            display.print_table(data, '')
+            save_raport(data)
+
+
+def save_raport(data):
+    if_want_save = 0
+    display.print_message("Do you want to save this raport? Press 1 to save...")
+    if_want_save = common.get_decision_input(if_want_save)
+    if if_want_save == 1:
+        filename = input("Enter filename")
+        data_manager.data_export(data, filename+".txt")
+
+def show_list_raport():
+    data = get_raport_list()
+    return data
+
+
+def delete_raport():
+    pass
