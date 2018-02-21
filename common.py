6d2698a9a03a50d8c0996aaa260090fd78ff7332
Damian <damian.targosz11@gmail.com>
2018-02-21 10:56:17 +0100

    Add import dates from file and change format in txt

--- common.py
+++ common.py
@@ -1,11 +1,12 @@
 import datetime
 import random
 import string
+import data_manager
 
-def sort():
 
-    myfile = [["xxx,xxx,shopp,xxx,2017,02,28"],["xxx,xxx,sx,xxx,2016,10,04"]]
-    list_to_check = split_elements(myfile)
+def sort(list_to_check):
+    """ Function display options sort by date or sort by category"""
+
     category = 2
     year = 4
     month = 5
@@ -14,10 +15,7 @@ def sort():
     sort_option = input("Do you want sort by date or category [d/c]?: ")
 
     if sort_option == "d":
-
         sort_date(list_to_check, year, month, day)
-
-
 
     elif sort_option == "c":
         get_category = input("Enter category: ") 
@@ -26,12 +24,12 @@ def sort():
             if elements[category] == get_category:
                 if sort_by_date == "n":
                     print(elements)
-
                 elif sort_by_date == "y":
                     sort_date(list_to_check, year, month, day)
                     
 
 def sort_date(list_to_check, year, month, day):
+    """ Function sort dates"""
 
     year_from = int(input("Give the year from [yyyy]: "))
     month_from = int(input("Give the month from [m]: "))
@@ -40,11 +38,11 @@ def sort_date(list_to_check, year, month, day):
     month_to = int(input("Give the month to [m]: "))
     day_to = int(input("Give the day to [d]: "))
 
-
     start = datetime.date(year_from,month_from,day_from) 
     end = datetime.date(year_to,month_to,day_to)
 
     for elements in list_to_check:
+        
         elements[year] = int(elements[year])
         elements[month] = int(elements[month])
         elements[day] = int(elements[day])
@@ -52,17 +50,8 @@ def sort_date(list_to_check, year, month, day):
         if set_year >= start and set_year <= end:
             print(elements)
 
-    
-def split_elements(myfile):
-    
-    list_to_check = []
-    for elements in myfile:
-        for i in elements:
-            i = i.split(",")
-            list_to_check.append(i)
-    return list_to_check
-
-sort()
+myfile = data_manager.data_import("expenses.txt")
+sort(myfile)
 
 def get_decision_input(dec):
     while dec is not True:
