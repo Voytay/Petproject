39bcf0207816f30d267e2a0365c945793d4a5c33
Jakub Wojtasik <jakubwojtasikzsp@gmail.com>
2018-02-21 13:53:40 +0100

    Delete fucntions

--- common.py
+++ common.py
@@ -12,18 +12,19 @@ def sort(list_to_check):
     month = 5
     day = 6
 
-    
+
 
     sort_option = input("Do you want sort by date or category [d/c]?: ")
     if sort_option == "d":
-        sort_date(list_to_check, year, month, day)
+        data = sort_date(list_to_check, year, month, day)
+        return data
 
     elif sort_option == "c":
         display_elements = []
 
-        get_category = input("Enter category: ") 
+        get_category = input("Enter category: ")
         sort_by_date = input("Do you want to display at specifc time [y/n]: ?")
-        for elements in list_to_check:             
+        for elements in list_to_check:
             if elements[category] == get_category:
                 if sort_by_date == "n":
                     display_elements.append(elements)
@@ -41,24 +42,22 @@ def sort_date(list_to_check, year, month, day):
     month_to = int(input("Give the month to [m]: "))
     day_to = int(input("Give the day to [d]: "))
 
-    start = datetime.date(year_from,month_from,day_from) 
+    start = datetime.date(year_from,month_from,day_from)
     end = datetime.date(year_to,month_to,day_to)
 
     display_elements = []
 
     for elements in list_to_check:
-        
+
         elements[year] = int(elements[year])
         elements[month] = int(elements[month])
         elements[day] = int(elements[day])
         set_year = datetime.date(elements[year],elements[month],elements[day])
         if set_year >= start and set_year <= end:
             display_elements.append(elements)
-            
+
     return display_elements
 
-myfile = data_manager.data_import("expenses.txt")
-sort(myfile)
 
 def get_decision_input(dec):
     while dec is not True:
@@ -66,19 +65,19 @@ def get_decision_input(dec):
             dec = int(input("Enter number: "))
             return dec
         except:
-            pass
+            print("Not number!")
 
 
 def generate_id(data):
-    sign = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "+"]  
+    sign = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "+"]
     unique = False
     while unique == False:
-        ids = "".join([random.choice(string.ascii_lowercase), 
+        ids = "".join([random.choice(string.ascii_lowercase),
                     random.choice(sign),
                     random.choice(string.digits),
                     random.choice(string.ascii_uppercase)])
         unique = True
-        
+
         ids_index = 0
         for row in data:
             if row[ids_index] == ids:
