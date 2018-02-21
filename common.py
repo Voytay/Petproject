4dbf77cffb83a2150baaa878ac35e0a048dbd439
Damian <damian.targosz11@gmail.com>
2018-02-21 13:04:52 +0100

    Add fix to common

--- common.py
+++ common.py
@@ -12,21 +12,24 @@ def sort(list_to_check):
     month = 5
     day = 6
 
-    sort_option = input("Do you want sort by date or category [d/c]?: ")
+    
 
+    sort_option = input("Do you want sort by date or category [d/c]?: ")
     if sort_option == "d":
         sort_date(list_to_check, year, month, day)
 
     elif sort_option == "c":
-        get_category = input("Enter category: ")
+        display_elements = []
+
+        get_category = input("Enter category: ") 
         sort_by_date = input("Do you want to display at specifc time [y/n]: ?")
-        for elements in list_to_check:
+        for elements in list_to_check:             
             if elements[category] == get_category:
                 if sort_by_date == "n":
-                    print(elements)
+                    display_elements.append(elements)
                 elif sort_by_date == "y":
                     sort_date(list_to_check, year, month, day)
-
+        return display_elements
 
 def sort_date(list_to_check, year, month, day):
     """ Function sort dates"""
@@ -38,39 +41,44 @@ def sort_date(list_to_check, year, month, day):
     month_to = int(input("Give the month to [m]: "))
     day_to = int(input("Give the day to [d]: "))
 
-    start = datetime.date(year_from,month_from,day_from)
+    start = datetime.date(year_from,month_from,day_from) 
     end = datetime.date(year_to,month_to,day_to)
 
+    display_elements = []
+
     for elements in list_to_check:
+        
         elements[year] = int(elements[year])
         elements[month] = int(elements[month])
         elements[day] = int(elements[day])
         set_year = datetime.date(elements[year],elements[month],elements[day])
         if set_year >= start and set_year <= end:
-            print(elements)
+            display_elements.append(elements)
+            
+    return display_elements
 
 myfile = data_manager.data_import("expenses.txt")
-#sort(myfile)
+sort(myfile)
 
-def get_decision_input(decision):
-    while decision is not True:
+def get_decision_input(dec):
+    while dec is not True:
         try:
-            decision = int(input("Enter number: "))
-            return decision
+            dec = int(input("Enter number: "))
+            return dec
         except:
             pass
 
 
 def generate_id(data):
-    sign = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "+"]
+    sign = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "+"]  
     unique = False
     while unique == False:
-        ids = "".join([random.choice(string.ascii_lowercase),
+        ids = "".join([random.choice(string.ascii_lowercase), 
                     random.choice(sign),
                     random.choice(string.digits),
                     random.choice(string.ascii_uppercase)])
         unique = True
-
+        
         ids_index = 0
         for row in data:
             if row[ids_index] == ids:
