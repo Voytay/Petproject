bd78ae246bae491b228293c2147b747db8d0063c
idzia <ania.idzi@gmail.com>
2018-02-21 11:40:01 +0100

    Add automatic column expansion

--- display.py
+++ display.py
@@ -16,13 +16,24 @@ def print_message(message):
 
 
 def print_table(data, title):
-    lenght = [6, 8, 15, 30, 6, 4, 4]
-    additional_characters = 16
-    width = sum(lenght) + additional_characters
-    row = []
     
+    max_lenght = []
     for i in range(len(title)):
-        row.append("| " + title[i].center(lenght[i]))
+        for j in range(len(data)):
+            max_len_in_column = len(title[i])
+
+            if len(data[j][i]) > max_len_in_column:
+                max_len_in_column = len(data[j][i])
+        
+        max_lenght.append(max_len_in_column)    
+    
+    extra_sign = 2*len(title)+2
+    
+    width = sum(max_lenght) + extra_sign
+
+    row = []
+    for i in range(len(title)):
+        row.append("| " + title[i].center(max_lenght[i]))
     row.append(" |")
     print("".join(row))
     print("="*width)
@@ -30,6 +41,13 @@ def print_table(data, title):
     for item in data:    
         row = []
         for i in range(len(title)):
-            row.append("| " + item[i].rjust(lenght[i]))
+            row.append("| " + item[i].rjust(max_lenght[i]))
         row.append(" |")
         print("".join(row))
+
+title = ["id", "kwota", "kategoria"]
+data = [["1","25.00","shopping"], 
+        ["2","5.00","transport"],
+        ["3","56.50","shopping"]]
+tabelka = print_table(data, title)
+
