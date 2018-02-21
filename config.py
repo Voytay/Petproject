a72f88c2c24137e49d2547ce30e200aba3599a4b
Jakub Wojtasik <jakubwojtasikzsp@gmail.com>
2018-02-21 15:54:38 +0100

    Showing/editing categories add

--- config.py
+++ config.py
@@ -8,31 +8,61 @@ Allows to:
 import display
 import common
 import data_manager
+import raports
 
 
 def config():
     decision = 0
-    while decision != 3:
+    while decision != 2:
         display.print_message("Config menu")
-        menu = ['Categories', ' Raports', 'Exit']
+        menu = ['Categories', 'Exit']
         display.print_menu(menu)
         decision = common.get_decision_input(decision)
         if decision == 1:
             categories()
-        elif decision == 2:
-            pass
+
 
 
 def categories():
     decision = 0
     while decision != 3:
-        decision = 0
-        display.print_message("Categories menu")
+        display.print_message("Config categories menu")
         menu = ['Show categories', 'Edit categories', 'Exit']
         display.print_menu(menu)
         decision = common.get_decision_input(decision)
         if decision == 1:
             cat = data_manager.data_import('categories.txt')
-            display.print_message(cat)
+            display.print_list(cat, "Categories")
         elif decision == 2:
-            pass
+            edit_categories()
+
+
+def edit_categories():
+    decision = 0
+    while decision != 3:
+        display.print_message("Edit categories")
+        menu = ['Add category', 'Delete category', 'Exit']
+        display.print_menu(menu)
+        decision = common.get_decision_input(decision)
+        if decision == 1:
+            add_category()
+        elif decision == 2:
+            remove_category()
+
+
+def add_category():
+    name = [input("Enter name of category: ")]
+    cat = data_manager.data_import('categories.txt')
+    cat.append(name)
+    data_manager.data_export(cat, 'categories.txt')
+
+
+def remove_category():
+    name = input("Enter category to remove: ")
+    cat = data_manager.data_import('categories.txt')
+    for row in cat:
+        for element in row:
+            if element == name:
+                to_remove = cat.index(row)
+                cat.pop(to_remove)
+    data_manager.data_export(cat, 'categories.txt')
